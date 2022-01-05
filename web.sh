66d249b751bbde674b581509ab70acc799ab56e9
sk33z3r <sk33z3r@protonmail.com>
2022-01-05 11:48:19 -0500

    chore[docker]: enchance local server script

--- web.sh
+++ web.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -d -p 8080:80 --name moonchild-site -v "$PWD":/var/www/html:ro php:7.2-apache
+docker rm -f moonchild-site
+docker run -d --rm -p 8080:80 --name moonchild-site -v "$PWD":/var/www/html:ro php:7.2-apache
